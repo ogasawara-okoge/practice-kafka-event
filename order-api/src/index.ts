772ae5db6fad1dec10a9e1e5ff31a35aa783d93b
ogasawara-okoge <bu.arca.10good@gmail.com>
2026-08-14 15:58:41 +0900

    apiにCORSの設定を追加

--- order-api/src/index.ts
+++ order-api/src/index.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 import { Kafka } from 'kafkajs';
 import { config } from './config.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // ★ config からポート番号を取得
 const PORT = config.port;
@@ -21,27 +23,28 @@ const run = async () => {
   console.log('Kafka Producer connected successfully.');
 
   app.post('/orders', async (req: Request, res: Response) => {
-    const orderData = req.body;
+    const orderData = req.body;// 注文IDをここで確定させる
     console.log('Received order request:', orderData);
-
+    const orderId = Date.now().toString();
+    const requestOrderData = {
+      orderId: orderId,
+      item: orderData.item || 'Default Item',
+      quantity: orderData.quantity || 1,
+      createdAt: new Date().toISOString(),
+    };
     try {
       // 4. Kafkaへの送信（configのトピック名を使用）
       await producer.send({
         topic: config.kafka.topic,
         messages: [
           {
-            value: JSON.stringify({
-              orderId: orderData.orderId || Date.now().toString(),
-              item: orderData.item || 'Default Item',
-              quantity: orderData.quantity || 1,
-              createdAt: new Date().toISOString(),
-            }),
+            value: JSON.stringify(requestOrderData),
           },
         ],
       });
 
       console.log('Order event published to Kafka successfully.');
-      res.status(201).json({ message: 'Order received and event published!', order: orderData });
+      res.status(201).json({ message: 'Order received and event published!', order: requestOrderData });
     } catch (error) {
       console.error('Failed to publish order event:', error);
       res.status(500).json({ error: 'Internal Server Error' });
